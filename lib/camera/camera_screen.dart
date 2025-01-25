4b0a2c396358e0611bd0c29c1dd6015224b31cc4
Kolade <kolademodupe007@gmail.com>
2025-01-25 11:59:57 +0100

    contd

--- lib/camera/camera_screen.dart
+++ lib/camera/camera_screen.dart
@@ -1,6 +1,6 @@
+import "package:camera/camera.dart";
 import "package:flutter/material.dart";
 import "package:get/get.dart";
-import "package:get/get_state_manager/src/simple/get_view.dart";
 import "package:tfliteappv3/scan_controller.dart";
 
 class CameraScreen extends GetView<ScanController> {
@@ -8,6 +8,11 @@ class CameraScreen extends GetView<ScanController> {
 
   @override
   Widget build(BuildContext context) {
-    return GetX(builder: (controller),);
+    return GetX<ScanController>(builder: (controller){
+      if (!controller.isInitialized){return Container();}
+      return MaterialApp(
+        home: CameraPreview(controller.cameraController),
+      );
+    });
   }
 }
--- lib/scan_controller.dart
+++ lib/scan_controller.dart
@@ -1,6 +1,40 @@
 
+// ignore_for_file: avoid_print
+
+import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 
 class ScanController extends GetxController{
 
+final RxBool _isInitialized = RxBool(false);
+bool get isInitialized => _isInitialized.value;
+late CameraController _cameraController;
+late List<CameraDescription> _cameras;
+CameraController get cameraController => _cameraController;
+
+
+Future<void> _initCamera() async {
+  _cameras = await availableCameras();
+  _cameraController = CameraController(_cameras[0], ResolutionPreset.max);
+  _cameraController.initialize().then((_) {
+   
+  }).catchError((Object e) {
+    if (e is CameraException){
+      switch (e.code){
+        case "CameraAccessDenied":
+        print ("User denied access to camera");
+        break;
+        default:
+        print("Handle other errors");
+        break;
+
+      }
+    } 
+  });
+}
+@override
+  void onInit() {
+    _initCamera();
+    super.onInit();
+  }
 }
